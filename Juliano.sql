00d154f6ca386b1dd7f4bef7017ee0cfeaf312c5
Juliano Araújo Cezarotto <145863641+Juliano1504@users.noreply.github.com>
2024-02-18 15:10:33 -0300

    Update Juliano.sql

--- Juliano.sql
+++ Juliano.sql
@@ -1,103 +1,103 @@
 CREATE TABLE Filmes ( Id NUMBER PRIMARY KEY, Nome VARCHAR2(120), DataLancamento DATE, Valor NUMBER(10,2), Genero VARCHAR2(120) );
 
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (1, 'O Poderoso Chef„o', TO_DATE('1972-03-24', 'YYYY-MM-DD'), 12.99, 'Drama');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (1, 'O Poderoso Chef√£o', TO_DATE('1972-03-24', 'YYYY-MM-DD'), 12.99, 'Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (2, 'Pulp Fiction: Tempo de ViolÍncia', TO_DATE('1994-10-14', 'YYYY-MM-DD'), 14.99, 'Crime, Drama');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (2, 'Pulp Fiction: Tempo de Viol√™ncia', TO_DATE('1994-10-14', 'YYYY-MM-DD'), 14.99, 'Crime, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (3, 'O Senhor dos AnÈis: O Retorno do Rei', TO_DATE('2003-12-17', 'YYYY-MM-DD'), 17.50, 'Aventura, Fantasia');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (3, 'O Senhor dos An√©is: O Retorno do Rei', TO_DATE('2003-12-17', 'YYYY-MM-DD'), 17.50, 'Aventura, Fantasia');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (4, 'Forrest Gump: O Contador de HistÛrias', TO_DATE('1994-07-06', 'YYYY-MM-DD'), 11.25, 'Drama, Romance');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (4, 'Forrest Gump: O Contador de Hist√≥rias', TO_DATE('1994-07-06', 'YYYY-MM-DD'), 11.25, 'Drama, Romance');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (5, 'Matrix', TO_DATE('1999-04-08', 'YYYY-MM-DD'), 9.99, 'AÁ„o, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (5, 'Matrix', TO_DATE('1999-04-08', 'YYYY-MM-DD'), 9.99, 'A√ß√£o, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (6, 'O SilÍncio dos Inocentes', TO_DATE('1991-02-14', 'YYYY-MM-DD'), 13.75, 'Crime, Drama, Suspense');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (6, 'O Sil√™ncio dos Inocentes', TO_DATE('1991-02-14', 'YYYY-MM-DD'), 13.75, 'Crime, Drama, Suspense');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (7, 'Interestelar', TO_DATE('2014-11-07', 'YYYY-MM-DD'), 16.80, 'Aventura, Drama, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (7, 'Interestelar', TO_DATE('2014-11-07', 'YYYY-MM-DD'), 16.80, 'Aventura, Drama, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (8, 'Gladiador', TO_DATE('2000-05-05', 'YYYY-MM-DD'), 12.50, 'AÁ„o, Aventura, Drama');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (8, 'Gladiador', TO_DATE('2000-05-05', 'YYYY-MM-DD'), 12.50, 'A√ß√£o, Aventura, Drama');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (9, 'O Resgate do Soldado Ryan', TO_DATE('1998-07-24', 'YYYY-MM-DD'), 14.25, 'Drama, Guerra');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (10, 'Os Bons Companheiros', TO_DATE('1990-09-21', 'YYYY-MM-DD'), 13.99, 'Biografia, Crime, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (11, 'A Origem', TO_DATE('2010-07-30', 'YYYY-MM-DD'), 15.50, 'AÁ„o, Aventura, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (11, 'A Origem', TO_DATE('2010-07-30', 'YYYY-MM-DD'), 15.50, 'A√ß√£o, Aventura, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (12, 'A Lista de Schindler', TO_DATE('1993-12-15', 'YYYY-MM-DD'), 12.25, 'Biografia, Drama, HistÛria');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (12, 'A Lista de Schindler', TO_DATE('1993-12-15', 'YYYY-MM-DD'), 12.25, 'Biografia, Drama, Hist√≥ria');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (13, 'Cidade de Deus', TO_DATE('2002-02-08', 'YYYY-MM-DD'), 11.99, 'Crime, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (14, 'O Grande Truque', TO_DATE('2006-11-10', 'YYYY-MM-DD'), 14.50, 'Drama, MistÈrio, Suspense');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (14, 'O Grande Truque', TO_DATE('2006-11-10', 'YYYY-MM-DD'), 14.50, 'Drama, Mist√©rio, Suspense');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (15, 'Scarface', TO_DATE('1983-12-09', 'YYYY-MM-DD'), 13.25, 'Crime, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (16, 'Laranja Mec‚nica', TO_DATE('1971-02-02', 'YYYY-MM-DD'), 10.99, 'Crime, Drama, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (16, 'Laranja Mec√¢nica', TO_DATE('1971-02-02', 'YYYY-MM-DD'), 10.99, 'Crime, Drama, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (17, 'Os Suspeitos', TO_DATE('1995-08-16', 'YYYY-MM-DD'), 12.80, 'Crime, Drama, MistÈrio');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (17, 'Os Suspeitos', TO_DATE('1995-08-16', 'YYYY-MM-DD'), 12.80, 'Crime, Drama, Mist√©rio');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (18, 'O Lobo de Wall Street', TO_DATE('2013-12-25', 'YYYY-MM-DD'), 15.25, 'Biografia, Crime, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (19, 'O Sexto Sentido', TO_DATE('1999-08-06', 'YYYY-MM-DD'), 11.99, 'Drama, MistÈrio, Suspense');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (19, 'O Sexto Sentido', TO_DATE('1999-08-06', 'YYYY-MM-DD'), 11.99, 'Drama, Mist√©rio, Suspense');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (20, 'Os Intoc·veis', TO_DATE('1987-06-03', 'YYYY-MM-DD'), 12.50, 'Biografia, Crime, Drama');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (20, 'Os Intoc√°veis', TO_DATE('1987-06-03', 'YYYY-MM-DD'), 12.50, 'Biografia, Crime, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (21, 'CoraÁ„o Valente', TO_DATE('1995-09-22', 'YYYY-MM-DD'), 14.75, 'Biografia, Drama, HistÛria');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (21, 'Cora√ß√£o Valente', TO_DATE('1995-09-22', 'YYYY-MM-DD'), 14.75, 'Biografia, Drama, Hist√≥ria');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (22, 'Clube da Luta', TO_DATE('1999-10-29', 'YYYY-MM-DD'), 13.80, 'Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (23, 'De Volta para o Futuro', TO_DATE('1985-07-03', 'YYYY-MM-DD'), 10.50, 'Aventura, ComÈdia, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (23, 'De Volta para o Futuro', TO_DATE('1985-07-03', 'YYYY-MM-DD'), 10.50, 'Aventura, Com√©dia, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (24, 'O Auto da Compadecida', TO_DATE('2000-09-10', 'YYYY-MM-DD'), 12.99, 'ComÈdia');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (24, 'O Auto da Compadecida', TO_DATE('2000-09-10', 'YYYY-MM-DD'), 12.99, 'Com√©dia');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (25, 'O Iluminado', TO_DATE('1980-06-13', 'YYYY-MM-DD'), 11.25, 'Drama, Terror');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (26, 'A Viagem de Chihiro', TO_DATE('2001-07-20', 'YYYY-MM-DD'), 14.50, 'AnimaÁ„o, Aventura, FamÌlia');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (26, 'A Viagem de Chihiro', TO_DATE('2001-07-20', 'YYYY-MM-DD'), 14.50, 'Anima√ß√£o, Aventura, Fam√≠lia');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (27, 'Vingadores: Ultimato', TO_DATE('2019-04-26', 'YYYY-MM-DD'), 17.25, 'AÁ„o, Aventura, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (27, 'Vingadores: Ultimato', TO_DATE('2019-04-26', 'YYYY-MM-DD'), 17.25, 'A√ß√£o, Aventura, Fic√ß√£o Cient√≠fica');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (28, 'Django Livre', TO_DATE('2013-01-18', 'YYYY-MM-DD'), 14.99, 'Drama, Faroeste');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (29, 'O Rei Le„o', TO_DATE('1994-07-15', 'YYYY-MM-DD'), 12.50, 'AnimaÁ„o, Aventura, Drama');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (29, 'O Rei Le√£o', TO_DATE('1994-07-15', 'YYYY-MM-DD'), 12.50, 'Anima√ß√£o, Aventura, Drama');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (30, 'Batman: O Cavaleiro das Trevas', TO_DATE('2008-07-18', 'YYYY-MM-DD'), 15.99, 'AÁ„o, Crime, Drama');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (30, 'Batman: O Cavaleiro das Trevas', TO_DATE('2008-07-18', 'YYYY-MM-DD'), 15.99, 'A√ß√£o, Crime, Drama');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (31, 'O Labirinto do Fauno', TO_DATE('2006-10-20', 'YYYY-MM-DD'), 13.75, 'Drama, Fantasia, Guerra');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (32, 'Tubar„o', TO_DATE('1975-06-20', 'YYYY-MM-DD'), 11.99, 'Aventura, Suspense');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (32, 'Tubar√£o', TO_DATE('1975-06-20', 'YYYY-MM-DD'), 11.99, 'Aventura, Suspense');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (33, 'Os CaÁadores da Arca Perdida', TO_DATE('1981-06-12', 'YYYY-MM-DD'), 12.50, 'Aventura');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (33, 'Os Ca√ßadores da Arca Perdida', TO_DATE('1981-06-12', 'YYYY-MM-DD'), 12.50, 'Aventura');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (34, 'E o Vento Levou', TO_DATE('1940-12-15', 'YYYY-MM-DD'), 10.99, 'Drama, Romance');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (35, 'O Show de Truman: O Show da Vida', TO_DATE('1998-09-04', 'YYYY-MM-DD'), 11.25, 'ComÈdia, Drama, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (35, 'O Show de Truman: O Show da Vida', TO_DATE('1998-09-04', 'YYYY-MM-DD'), 11.25, 'Com√©dia, Drama, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (36, 'Whiplash: Em Busca da PerfeiÁ„o', TO_DATE('2015-02-08', 'YYYY-MM-DD'), 14.50, 'Drama, M˙sica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (36, 'Whiplash: Em Busca da Perfei√ß√£o', TO_DATE('2015-02-08', 'YYYY-MM-DD'), 14.50, 'Drama, M√∫sica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (37, 'O Fabuloso Destino de AmÈlie Poulain', TO_DATE('2001-04-25', 'YYYY-MM-DD'), 13.80, 'ComÈdia, Romance');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (37, 'O Fabuloso Destino de Am√©lie Poulain', TO_DATE('2001-04-25', 'YYYY-MM-DD'), 13.80, 'Com√©dia, Romance');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (38, 'O Pianista', TO_DATE('2002-01-24', 'YYYY-MM-DD'), 14.25, 'Biografia, Drama, Guerra');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (39, 'A Lista de Schindler', TO_DATE('1993-12-15', 'YYYY-MM-DD'), 12.25, 'Biografia, Drama, HistÛria');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (39, 'A Lista de Schindler', TO_DATE('1993-12-15', 'YYYY-MM-DD'), 12.25, 'Biografia, Drama, Hist√≥ria');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (40, 'O Artista', TO_DATE('2012-02-10', 'YYYY-MM-DD'), 13.99, 'ComÈdia, Drama, Romance');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (40, 'O Artista', TO_DATE('2012-02-10', 'YYYY-MM-DD'), 13.99, 'Com√©dia, Drama, Romance');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (41, 'A Origem', TO_DATE('2010-07-30', 'YYYY-MM-DD'), 15.50, 'AÁ„o, Aventura, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (41, 'A Origem', TO_DATE('2010-07-30', 'YYYY-MM-DD'), 15.50, 'A√ß√£o, Aventura, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (42, 'O Fabuloso Destino de AmÈlie Poulain', TO_DATE('2001-04-25', 'YYYY-MM-DD'), 13.80, 'ComÈdia, Romance');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (42, 'O Fabuloso Destino de Am√©lie Poulain', TO_DATE('2001-04-25', 'YYYY-MM-DD'), 13.80, 'Com√©dia, Romance');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (43, 'Interestelar', TO_DATE('2014-11-07', 'YYYY-MM-DD'), 16.80, 'Aventura, Drama, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (43, 'Interestelar', TO_DATE('2014-11-07', 'YYYY-MM-DD'), 16.80, 'Aventura, Drama, Fic√ß√£o Cient√≠fica');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (44, 'O Pianista', TO_DATE('2002-01-24', 'YYYY-MM-DD'), 14.25, 'Biografia, Drama, Guerra');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (45, 'A Lista de Schindler', TO_DATE('1993-12-15', 'YYYY-MM-DD'), 12.25, 'Biografia, Drama, HistÛria');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (45, 'A Lista de Schindler', TO_DATE('1993-12-15', 'YYYY-MM-DD'), 12.25, 'Biografia, Drama, Hist√≥ria');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (46, 'O Artista', TO_DATE('2012-02-10', 'YYYY-MM-DD'), 13.99, 'ComÈdia, Drama, Romance');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (46, 'O Artista', TO_DATE('2012-02-10', 'YYYY-MM-DD'), 13.99, 'Com√©dia, Drama, Romance');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (47, 'A Origem', TO_DATE('2010-07-30', 'YYYY-MM-DD'), 15.50, 'AÁ„o, Aventura, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (47, 'A Origem', TO_DATE('2010-07-30', 'YYYY-MM-DD'), 15.50, 'A√ß√£o, Aventura, Fic√ß√£o Cient√≠fica');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (48, 'O Fabuloso Destino de AmÈlie Poulain', TO_DATE('2001-04-25', 'YYYY-MM-DD'), 13.80, 'ComÈdia, Romance');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (48, 'O Fabuloso Destino de Am√©lie Poulain', TO_DATE('2001-04-25', 'YYYY-MM-DD'), 13.80, 'Com√©dia, Romance');
 
-INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (49, 'Interestelar', TO_DATE('2014-11-07', 'YYYY-MM-DD'), 16.80, 'Aventura, Drama, FicÁ„o CientÌfica');
+INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (49, 'Interestelar', TO_DATE('2014-11-07', 'YYYY-MM-DD'), 16.80, 'Aventura, Drama, Fic√ß√£o Cient√≠fica');
 
 INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (50, 'O Pianista', TO_DATE('2002-01-24', 'YYYY-MM-DD'), 14.25, 'Biografia, Drama, Guerra');
 
@@ -111,7 +111,7 @@ INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (50, 'O Pian
 
 4- Select * from filmes where valor > 15
 
-5- Select * from filmes where genero like '%AÁ„o%'
+5- Select * from filmes where genero like '%A√ß√£o%'
 
 6- Select * from filmes where genero like '%Drama%' or genero like '%Suspense%'
 
@@ -123,15 +123,15 @@ INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (50, 'O Pian
 
 10- Select * from filmes order by genero     Select genero, count(*) as totalfilmes from filmes group by genero
 
-11- Select count( distinct nome) as totalfilmes from filmes
+11- Select count(distinct nome) as totalfilmes from filmes
 
-12- Select * from filmes where datalancamento = ( select min(datalancamento) from filmes)
+12- Select min(DataLancamento) from filmes
 
-13- Select avg (valor) as MediaValor from filmes
+13- Select avg(valor) as MediaValor from filmes
 
 14- Select * from filmes order by datalancamento desc
 
-15- Select * from filmes where extract ( year from DataLancamento) = 1999 and valor >10
+15- Select * from filmes where extract( year from DataLancamento) = 1999 and valor >10
 
 16- Select * from filmes where nome like '%Senhor%'
 
@@ -145,24 +145,14 @@ INSERT INTO Filmes (Id, Nome, DataLancamento, Valor, Genero) VALUES (50, 'O Pian
 
 21- Select * from filmes where valor between 10 and 20
 
-22- Select * from filmes where to_char(datalancamento, 'DY', 'NLS_date_language=english') in ('MON', 'TUE', 'WED', 'THU', 'FRI')
+22- Select * from filmes where to_char(datalancamento, 'Dy') not in ('Sab', 'Dom')
 
-23- Select to_char(DataLancamento, 'YYYY') as anolancamento, count(*) as quantidade 
-from filmes
-group by to_char(dataLancamento, 'YYYY')
-order by to_char(dataLancamento, 'YYYY')
+23- Select extract(year from DataLancamento), count(extract(year from DataLancamento)) from filmes group by extract(year from DataLancamento)
 
 24- Select * from filmes where length(nome) >= 10
 
 25- Select * from filmes where genero is null
-    Select * from filmes where instr(genero, ',') > 0 ---- instr verifica informaÁıes em determinada string, no caso estou verificando see h· uma linha na sring genero
-
-
-
-
-
-
-
+    Select * from filmes where instr(genero, ',') > 0 ---- instr verifica informa√ß√µes em determinada string, no caso estou verificando see h√° uma linha na sring genero
 
 
 
