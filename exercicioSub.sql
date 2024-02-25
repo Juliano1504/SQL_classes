85f235ed9381fec38bc94a388e6c2d9d5e94d557
Juliano Araújo Cezarotto <145863641+Juliano1504@users.noreply.github.com>
2024-02-25 15:50:27 -0300

    Update exercicioSub.sql

--- exercicioSub.sql
+++ exercicioSub.sql
@@ -10,5 +10,6 @@ where endereco_id = (select id from enderecos where cidade = 'Nova Esperança')
 4- select nome, idade from pessoas
 where endereco_id = (select id from enderecos  where rua = 'Rua das Acácias')
 
-5- select nome from pessoas
-where endereco_id in(select cidade from enderecos group by cidade having count(nome) >=3)
+5- select * from pessoas
+where endereco_id in (select e.id from enderecos e inner join pessoas p on e.id = p.endereco_id group by cidade, e.id having count(cidade) > 3)
+
